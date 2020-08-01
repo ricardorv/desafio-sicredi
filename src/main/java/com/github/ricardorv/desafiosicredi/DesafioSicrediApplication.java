38546b8f0fd6b3a3c9cf3c95d82aaa734ec42705
Ricardo Rotta Valezan <ricardovalezan@gmail.com>
2020-08-01 19:56:19 -0300

    spring initializr

---
+++ src/main/java/com/github/ricardorv/desafiosicredi/DesafioSicrediApplication.java
@@ -0,0 +1,13 @@
+package com.github.ricardorv.desafiosicredi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioSicrediApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioSicrediApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/ricardorv/desafiosicredi/DesafioSicrediApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ricardorv.desafiosicredi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioSicrediApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
